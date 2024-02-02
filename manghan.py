09b83525a9d18693710a642d57d6681e545d53ae
marc <schoenigm.tin23@student.dhbw-heidenheim.de>
2024-02-02 10:11:14 +0100

    gpio

--- manghan.py
+++ manghan.py
@@ -1,4 +1,15 @@
 import random
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode (GPIO.BCM)
+GPIO.setup (17, GPIO.OUT)
+
+while True:
+    GPIO.output(17, True)
+    time.sleep(1)
+    GPIO.output(17, False)
+    time.sleep(1)
 
 words = [
     "Apfel", "Banane", "Kirsche", "Orange", "Erdbeere",
