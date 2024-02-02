4020310c6c63c9b318b6ab60b4ea65fba0be1a45
marc <schoenigm.tin23@student.dhbw-heidenheim.de>
2024-02-02 10:19:27 +0100

    updated

--- manghan.py
+++ manghan.py
@@ -3,12 +3,12 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode (GPIO.BCM)
-GPIO.setup (17, GPIO.OUT)
+GPIO.setup (2, GPIO.OUT)
 
 while True:
-    GPIO.output(17, True)
+    GPIO.output(2, True)
     time.sleep(1)
-    GPIO.output(17, False)
+    GPIO.output(2, False)
     time.sleep(1)
 
 words = [
